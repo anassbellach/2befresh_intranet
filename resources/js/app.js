930dd0d9caaa7ca8558c6e1500ddf7e38679f167
anassbellach1 <anass@2befresh.nl>
2025-01-16 22:15:46 +0100

    new commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,15 +6,13 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import MainLayout from "@/Layouts/MainLayout.vue";
 
 createInertiaApp({
-    resolve: async name => {
-        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true });
-        const page = await pages[`./Pages/${name}.vue`];
-
-        // Apply MainLayout only if `layout` is undefined (not null)
-        page.default.layout = page.default.layout === undefined ? MainLayout : page.default.layout;
-
-        return page;
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+        let page = pages[`./Pages/${name}.vue`]
+        page.default.layout = page.default.layout || MainLayout
+        return page
     },
+
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
