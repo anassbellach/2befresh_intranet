2079ac4ea7e1009e939cb0d19dfe7ded69e57f4e
anassbellach1 <anass@2befresh.nl>
2025-01-17 12:35:29 +0100

    new commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,10 +9,11 @@ createInertiaApp({
     resolve: name => {
         const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
         let page = pages[`./Pages/${name}.vue`]
-        page.default.layout = page.default.layout || MainLayout
+
+        // Apply MainLayout only if `layout` is undefined (not null)
+        page.default.layout = page.default.layout === undefined ? MainLayout : page.default.layout;
         return page
     },
-
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
