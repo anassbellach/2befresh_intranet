1e2fec3c1268e8f80e93d7bd7f7d51ad3e36b7a9
anassbellach1 <anass@2befresh.nl>
2025-01-23 11:18:49 +0100

    Fixed error fallback for klanten and leveranciers statistieken

--- resources/js/Pages/Leveranciers/Statistieken/Index.vue
+++ resources/js/Pages/Leveranciers/Statistieken/Index.vue
@@ -8,7 +8,7 @@
                 </p>
 
                 <!-- Fallback if no data -->
-                <div v-if="Object.keys(klanten_growth_by_year).length === 0" class="text-center text-gray-500 p-6">
+                <div v-if="Object.keys(leveranciers_per_plaats).length === 0" class="text-center text-gray-500 p-6">
                     <i class="fas fa-chart-line text-gray-300 text-6xl mb-4"></i>
                     <p class="text-lg font-medium">Geen gegevens beschikbaar om de grafiek weer te geven.</p>
                 </div>
